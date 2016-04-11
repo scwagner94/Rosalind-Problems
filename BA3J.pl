f1c43a7004a301c2d49bddf057c84b7601d673f3
Sean Wagner <scwagner94@gmail.com>
2016-04-11 10:14:41 -0400

    added files for two additional unsolved solutions

---
+++ BA3J.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+#Sean Wagner
+#BA3J.pl
+#paired read reconstruction to determine the original string
+
+use	strict;
+use	warnings;
+use Data::Dumper qw(Dumper);
---
+++ BA3K.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+#Sean Wagner
+#BA3K.pl
+#generate contigs given an input list of kmers
+
+use	strict;
+use	warnings;
+use Data::Dumper qw(Dumper);
